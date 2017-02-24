5cade04f6975d6399487be1cb73b3e79b1ff5f35
Sean Martz <martzy13@gmail.com>
2017-02-24 09:10:35 -0500

    add end time

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
 var countdown;
 var timeLeft = document.querySelector('.time-left');
+var endTime = document.querySelector('.time-end');
 
 function timer(seconds) {
     const now = Date.now(); // milliseconds
     const then = now + (seconds * 1000); // figure out end time in milliseconds
     displayTimeLeft(seconds);
+    displayEndTime(then);
+
     countdown = setInterval(function () {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
         if (secondsLeft < 0) {
@@ -18,7 +21,17 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
     const minutes = Math.floor(seconds / 60);
     const secondsLeft = Math.round(seconds % 60);
-    const display = `${minutes}:${secondsLeft}`;
+    const display = `${minutes}:${secondsLeft < 10 ? '0' : ''}${secondsLeft}`;
     timeLeft.textContent = display;
-    console.log(minutes, ':', secondsLeft, seconds);
+    document.title = display;
+
+
+}
+
+function displayEndTime(timestamp) {
+    var q = new Date(timestamp);
+    console.log(timestamp);
+    const hours = q.getHours();
+    const minutes = q.getMinutes();
+    endTime.textContent = `${hours > 12 ? hours - 12 : hours}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
